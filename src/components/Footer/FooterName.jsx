f2b45e0f8e55514d45bb11947cc2e386a325666d
ayushman <ayushmansingh2512@gmail.com>
2026-01-14 19:58:17 +0530

    added fotter

--- src/components/Footer/FooterName.jsx
+++ src/components/Footer/FooterName.jsx
@@ -5,9 +5,9 @@ const FooterName = () => {
         <div className="footer-content">
             <motion.h1
                 className="footer-name"
-                initial={{ y: 100, opacity: 0 }}
+                initial={{ y: 50, opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true, amount: 0.3 }}
+                viewport={{ once: true, amount: 0.1 }}
                 transition={{ duration: 1.2, ease: "easeOut" }}
             >
                 Aditya PS.
